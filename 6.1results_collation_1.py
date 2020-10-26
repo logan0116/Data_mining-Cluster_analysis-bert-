c6346e2e58eaf2db51331847735bba37bc50ae2b
Logan <33882477+logan0116@users.noreply.github.com>
2020-10-27 07:35:58 +0800

    Update 6.1results_collation_1.py

--- 6.1results_collation_1.py
+++ 6.1results_collation_1.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     score = calinski_harabasz_score(text_matrix, k_means.labels_)
     print('数据聚%d类calinski_harabaz指数为：%f' % (cluster_num, score))
     cluster_list = a = [[] for i in range(cluster_num)]
-    csv_read = csv.reader(open('词向量预处理数据_1/yjm_20201023_2_span.csv', 'r', encoding='UTF-8'))
+    csv_read = csv.reader(open('词向量预处理数据_1/test_2_span.csv', 'r', encoding='UTF-8'))
     txt_list = []
     for each_record in csv_read:
         txt_list.append(each_record[1:])
@@ -21,6 +21,6 @@ if __name__ == '__main__':
         cluster_list[k_means.labels_[i]].append(txt_list[i])
 
     for i in range(len(cluster_list)):
-        csv_write = csv.writer(open('cluster_20201023/cluster_' + str(i) + '.csv', 'w', encoding='UTF-8', newline=''))
+        csv_write = csv.writer(open('cluster/cluster_' + str(i) + '.csv', 'w', encoding='UTF-8', newline=''))
         for each_record in cluster_list[i]:
             csv_write.writerow(each_record)
